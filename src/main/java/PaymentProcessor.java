741b7ea1ab7599ff6e90aa6c7069c4f030d793eb
Junior Rodríguez <juniorsrodriguez@unicesar.edu.co>
2025-04-27 14:51:15 -0500

    Actualización de Rutas

--- src/main/java/PaymentProcessor.java
+++ src/main/java/PaymentProcessor.java
@@ -2,10 +2,28 @@ package main.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PaymentProcessor {
     public static void main(String[] args) {
         SpringApplication.run(PaymentProcessor.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas las rutas
+                        .allowedOrigins("http://localhost:3000") // Origen permitido
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Cabeceras permitidas
+                        .allowCredentials(true); // Permitir credenciales (cookies, cabeceras de autorización, etc.)
+            }
+        };
+    }
+
 }
